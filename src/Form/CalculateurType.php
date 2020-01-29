5fc8e8b32455987bbd245e2e6a128cfb2a019118
THOMAS MAIRE <thomas.maire@cheops.fr>
2020-01-29 15:24:50 +0100

    finalisation du calculateur

--- src/Form/CalculateurType.php
+++ src/Form/CalculateurType.php
@@ -7,34 +7,65 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CalculateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('prix', IntegerType::class, array(
-                    'label' => "Prix de l'annonce"
+                    'label' => "Prix d'achat",
+                    'attr' => array(
+                        'placeholder' => "Ex: 150000"
+                    )
                 ))
                 ->add('loyer', IntegerType::class, array(
-                    'label' => "Loyer mensuel estimés"
+                    'label' => "Loyer mensuel estimés",
+                    'attr' => array(
+                        'placeholder' => "Ex: 800"
+                    )
                 ))
                 ->add('metreCarre', IntegerType::class, array(
-                    'label' => "m²"
-                ))
-                ->add('priceMetreCarre', IntegerType::class, array(
-                    'label' => "null"
+                    'label' => "m²",
+                    'attr' => array(
+                        'placeholder' => "Ex: 100"
+                    )
                 ))
                 ->add('apport', IntegerType::class, array(
-                    'label' => "Apport"
+                    'label' => "Apport",
+                    'attr' => array(
+                        'placeholder' => "Ex: 10000"
+                    )
                 ))
                 ->add('fraisNotaire', IntegerType::class, array(
-                    'label' => "Frais de notaire"
+                    'label' => "Frais de notaire",
+                    'attr' => array(
+                        'placeholder' => "Ex: 12330"
+                    )
+                ))
+                ->add('fraisNotaireEstime', TextType::class, array(
+                    'label' => null,
+                    'attr' => array(
+                        'readonly' => 'readonly'
+                    )
                 ))
                 ->add('garantie', IntegerType::class, array(
-                    'label' => "Garantie banque"
+                    'label' => "Garantie banque",
+                    'attr' => array(
+                        'placeholder' => "Ex: 4073"
+                    )
+                ))
+                ->add('garantieEstimation', TextType::class, array(
+                    'label' => null,
+                    'attr' => array(
+                        'readonly' => 'readonly'
+                    )
                 ))
                 ->add('travaux', IntegerType::class, array(
-                    'label' => "Travaux"
+                    'label' => "Travaux",
+                    'attr' => array(
+                        'placeholder' => "Ex: 50400"
+                    )
                 ))
                 ->add('travauxAide', ChoiceType::class, array(
                     'label' => null,
