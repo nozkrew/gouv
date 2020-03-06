16753efa84bd8a2b4823a3bb71d61e462b88e5c4
THOMAS MAIRE <thomas.maire@cheops.fr>
2020-03-06 15:44:13 +0100

    Ajout stratégie recherche

--- src/Twig/StrategyExtension.php
+++ src/Twig/StrategyExtension.php
@@ -14,13 +14,13 @@ class StrategyExtension extends AbstractExtension{
         ];
     }
     
-    public function getText(Strategy $strategy){
+    public function getText(Strategy $strategy, $breakline = true){
         
         $text = "Ma stratégie est de louer en ";
-        $text .= "<b>".implode(" et/ou en ", array_map(function($exploitation){ return $exploitation->getText(); }, $strategy->getExploitations()->toArray()))."</b>.<br>";
-        $text .= "Le type de bien sera <b>".implode(" ou ", array_map(function($type){ return $type->getText(); }, $strategy->getTypes()->toArray()))."</b>.<br>";
-        $text .= "Pour les travaux, <b>".$strategy->getTravaux()->getText()."</b> sera prévu(e).<br>";
-        $text .= "Le montant total de mon projet sera de <b>".$strategy->getPrice()."€</b>.<br>";
+        $text .= "<b>".implode(" et/ou en ", array_map(function($exploitation){ return $exploitation->getText(); }, $strategy->getExploitations()->toArray()))."</b>."; $breakline ? $text .= "<br>" : $text .= " ";
+        $text .= "Le type de bien sera <b>".implode(" ou ", array_map(function($type){ return $type->getText(); }, $strategy->getTypes()->toArray()))."</b>."; $breakline ? $text .= "<br>" : $text .= " ";
+        $text .= "Pour les travaux, <b>".$strategy->getTravaux()->getText()."</b> sera prévu(e)."; $breakline ? $text .= "<br>" : $text .= " ";
+        $text .= "Le montant total de mon projet sera de <b>".$strategy->getPrice()."€</b>."; $breakline ? $text .= "<br>" : $text .= " ";
         $text .= "Avec cet investissement, je souhaite dégager <b>".$strategy->getCashflow()."€</b> de cashflow mensuel.";
         
         return $text;
