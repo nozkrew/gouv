b96ffc6bc0e1b046245ee223457fe511a15b7208
THOMAS MAIRE <thomas.maire@cheops.fr>
2020-04-03 12:48:36 +0200

    Ajout partie recherche immo

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -15,8 +15,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 */
 class SearchController extends AbstractController
 {
+    
+    private const LINKS_DEMO = [
+        "https://www.laforet.com/acheter/rechercher?filter%5Bcities%5D=40088&filter%5Bmax%5D=200000&filter%5Bsurface%5D=80",
+        "https://www.bourse-immobilier.fr/achat-maison-appartement-immeuble-dax?quartiers=40088&surface=80&sterr=0&prix=-200000&typebien=1-2-9&nbpieces=1-2-3-4-5&og=0&where=Dax-__40100_&_b=1&_p=0&tyloc=2&neuf=1&ancien=1&ids=40088",
+        "https://www.cabinet-bedin.com/recherche/achat/dax-40100/maison,appartement,immeuble-mixte?prix-max=200000&surface-min=80"
+    ];
+
+
     /**
      * @Route("/")
+     * @Route("/demo")
      */
     public function index(Request $request)
     {
@@ -25,24 +34,8 @@ class SearchController extends AbstractController
         if($security->isGranted('IS_AUTHENTICATED_FULLY')){
             $searches = $this->getSearchRepository()->findByUser($this->getUser());
         }
-        else{
-            
-            $dax = $this->getCitiesRepository()->findOneBySlug('dax');
-            
-            $search = new Search();
-            $search->setName('[Exemple] Colocation à Dax');
-            $search->setPriceMax(200000);
-            $search->setSurfaceMin(80);
-            $search->addCity($dax);
-            $search->setLinks(array(
-                "https://www.avendrealouer.fr/recherche.html?pageIndex=1&pageSize=25&sortPropertyName=ReleaseDate&sortDirection=Descending&searchTypeID=1&typeGroupCategoryID=1&localityIds=101-18798&typeGroupIds=1,2,10,11,12&maximumPrice=200000&minimumSurface=80&hashSearch=null_null_null_null_200000_null_null_null_80_False__null_1_1_False_False______101-18798_____1,2,10,11,12_&UserSorted=true",
-                "https://www.century21.fr/annonces/achat/cpv-40100_dax/s-80-/st-0-/b-0-200000/page-1/"
-            ));
-            
-            $em = $this->getDoctrine()->getManager();
-            $em->detach($search);
-            
-            $searches = array($search);
+        else{            
+            $searches = array($this->getDemoSearch());
         }
         
         return $this->render('search/index.html.twig', [
@@ -123,8 +116,21 @@ class SearchController extends AbstractController
     
     /**
      * @Route("/{id}/lien/{index}", requirements={"id"="\d+", "index"="\d+"})
+     * @Route("/demo/lien/{index}", requirements={"index"="\d+"})
      */
-    public function lien(Request $request, $id, $index){
+    public function lien(Request $request, $id = null, $index){
+        
+        if($request->get('_route') == 'app_search_lien_1'){
+            
+            return $this->render('search/lien.html.twig', [
+                'search' => $this->getDemoSearch(),
+                'url' => self::LINKS_DEMO[$index],
+                'index' => $index,
+                'next' => isset(self::LINKS_DEMO[$index + 1]) ? true : false,
+                'previous' => isset(self::LINKS_DEMO[$index - 1]) ? true : false,
+            ]);
+        }
+        
         $search = $this->getSearchRepository()->findOneById($id);
         
         if($search === null || $search->getUser() !== $this->getUser()){
@@ -140,10 +146,28 @@ class SearchController extends AbstractController
         return $this->render('search/lien.html.twig', [
             'search' => $search,
             'url' => $search->getLinks()[$index],
-            'index' => $index
+            'index' => $index,
+            'next' => isset($search->getLinks()[$index + 1]) ? true : false,
+            'previous' => isset($search->getLinks()[$index - 1]) ? true : false,
         ]);
     }
 
+    private function getDemoSearch():Search {
+        
+        $dax = $this->getCitiesRepository()->findOneBySlug('dax');
+        
+        $search = new Search();
+        $search->setName('[Exemple] Colocation à Dax');
+        $search->setPriceMax(200000);
+        $search->setSurfaceMin(80);
+        $search->addCity($dax);
+        $search->setLinks(self::LINKS_DEMO);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->detach($search);
+        
+        return $search;
+    }
 
     private function getCitiesRepository(){
         return $this->container->get('doctrine')->getRepository(Cities::class);
